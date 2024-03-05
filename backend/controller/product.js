85f84fdc55af6026106dccfa1165256ec7578c16
AliSinno <alisinno26@gmail.com>
2024-03-05 09:48:29 +0200

    added detailed routes

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -1,10 +1,15 @@
 //here we handle CRUD api requests
 const handleList = async(req, res) =>{
-    console.log("sent a get request to Product controller")
+    console.log("sent a get request to Product controller");
+    
+    return res.status(200).json({
+        'message': 'sent a get request to products'
+    });
 };
 
 const handleGetById = async (req, res) => {
     console.log("get by ID");
+    
 };
 
 const handleCreate = async (req, res) =>{
--- backend/route/ordersRoute.js
+++ backend/route/ordersRoute.js
@@ -4,12 +4,12 @@ const controller = require('../controller/orders');
 
 
 //get list of all orders
-router.get('/',controller.handleList);
+router.get('/orders/',controller.handleList);
 //get order by id
-router.get('/:orderid',controller.handleGetById);
+router.get('/orders/:orderid',controller.handleGetById);
 //create a new order
-router.post('/',controller.handleCreate);
+router.post('/orders/',controller.handleCreate);
 //delete an order by id
-router.delete('/',controller.handleDelete);
+router.delete('/orders/',controller.handleDelete);
 
 module.exports = router;
